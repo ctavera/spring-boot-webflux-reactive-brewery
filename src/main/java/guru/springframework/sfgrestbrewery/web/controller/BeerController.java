127b577b21550abfef907f4f26964c08dfd54e1d
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-08-12 14:17:14 -0500

    43. Assignment 4 - Update Get Operations to Reactive Types. Closes #4

--- src/main/java/guru/springframework/sfgrestbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/sfgrestbrewery/web/controller/BeerController.java
@@ -29,11 +29,11 @@ public class BeerController {
     private final BeerService beerService;
 
     @GetMapping(produces = { "application/json" }, path = "beer")
-    public ResponseEntity<BeerPagedList> listBeers(@RequestParam(value = "pageNumber", required = false) Integer pageNumber,
-                                                   @RequestParam(value = "pageSize", required = false) Integer pageSize,
-                                                   @RequestParam(value = "beerName", required = false) String beerName,
-                                                   @RequestParam(value = "beerStyle", required = false) BeerStyleEnum beerStyle,
-                                                   @RequestParam(value = "showInventoryOnHand", required = false) Boolean showInventoryOnHand){
+    public ResponseEntity<Mono<BeerPagedList>> listBeers(@RequestParam(value = "pageNumber", required = false) Integer pageNumber,
+                                                         @RequestParam(value = "pageSize", required = false) Integer pageSize,
+                                                         @RequestParam(value = "beerName", required = false) String beerName,
+                                                         @RequestParam(value = "beerStyle", required = false) BeerStyleEnum beerStyle,
+                                                         @RequestParam(value = "showInventoryOnHand", required = false) Boolean showInventoryOnHand) {
 
         if (showInventoryOnHand == null) {
             showInventoryOnHand = false;
@@ -49,7 +49,7 @@ public class BeerController {
 
         BeerPagedList beerList = beerService.listBeers(beerName, beerStyle, PageRequest.of(pageNumber, pageSize), showInventoryOnHand);
 
-        return new ResponseEntity<>(beerList, HttpStatus.OK);
+        return ResponseEntity.ok(Mono.just(beerList));
     }
 
     @GetMapping("beer/{beerId}")
@@ -63,8 +63,8 @@ public class BeerController {
     }
 
     @GetMapping("beerUpc/{upc}")
-    public ResponseEntity<BeerDto> getBeerByUpc(@PathVariable("upc") String upc){
-        return new ResponseEntity<>(beerService.getByUpc(upc), HttpStatus.OK);
+    public ResponseEntity<Mono<BeerDto>> getBeerByUpc(@PathVariable("upc") String upc){
+        return ResponseEntity.ok(Mono.just(beerService.getByUpc(upc)));
     }
 
     @PostMapping(path = "beer")
